05d916851a89c05603462cf84cc533de0c2cfc27
Serhii <ya.motr@gmail.com>
2020-04-20 15:59:04 +0300

    DZ_1: Functional components

---
+++ react_dz/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyInfoComponent from "./components/MyInfoComponent";
+
+ReactDOM.render(
+  <MyInfoComponent />,
+  document.getElementById('root')
+);
